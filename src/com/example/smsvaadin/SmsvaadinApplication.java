55361f1a0092a2766c62c0e408d806f3cb34ff67
bibi <moi@email.com>
2011-11-06 16:34:16 +0100

    event

--- src/com/example/smsvaadin/SmsvaadinApplication.java
+++ src/com/example/smsvaadin/SmsvaadinApplication.java
@@ -1,5 +1,7 @@
 package com.example.smsvaadin;
 
+import sms.view.Windowsms;
+
 import com.vaadin.Application;
 import com.vaadin.ui.*;
 
@@ -7,9 +9,16 @@ public class SmsvaadinApplication extends Application {
 	@Override
 	public void init() {
 		Window mainWindow = new Window("Smsvaadin Application");
-		Label label = new Label("Hello Vaadin user");
-		mainWindow.addComponent(label);
+//		VerticalLayout vert = new VerticalLayout();
+//        vert.setEnabled(true);
+		Windowsms win = new Windowsms();
+		win.setHeight("100%");
+		win.setSizeFull();
+		//vert.addComponent(win);
+		mainWindow.addComponent(win);
+		mainWindow.setSizeFull();
 		setMainWindow(mainWindow);
+		
 	}
 
 }
